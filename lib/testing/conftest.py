ae41bbde7a8e8df48f52d6bd012004fd54dcbd08
Mr.Perfect 092 <135037455+stephan-maina@users.noreply.github.com>
2023-09-06 12:30:06 +0300

    Completed.

--- lib/testing/conftest.py
+++ lib/testing/conftest.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python3
+import pytest
+from MySet import MySet
 
-def pytest_itemcollected(item):
-    par = item.parent.obj
-    node = item.obj
-    pref = par.__doc__.strip() if par.__doc__ else par.__class__.__name__
-    suf = node.__doc__.strip() if node.__doc__ else node.__name__
-    if pref or suf:
-        item._nodeid = ' '.join((pref, suf))
+@pytest.fixture
+def empty_set():
+    return MySet()
+
+@pytest.fixture
+def sample_set():
+    s = MySet()
+    s.add(1)
+    s.add(2)
+    s.add(3)
+    return s

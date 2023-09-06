76924eb854d65e28d264e1b9db8c462324c77c09
Mr.Perfect 092 <135037455+stephan-maina@users.noreply.github.com>
2023-09-06 12:29:04 +0300

    Completed

--- lib/testing/set_test.py
+++ lib/testing/set_test.py
@@ -1,54 +1,43 @@
-#!/usr/bin/env python3
+import pytest
 from MySet import MySet
 
-class TestSet:
+def test_add_element(empty_set):
+    empty_set.add(1)
+    assert empty_set.contains(1)
 
-    def test_init(self):
-        '''Test __init__ set with list'''
-        test_set = MySet([1,2,3,4])
-        set_list = [1,2,3,4]
-        for num in set_list:
-            assert(num in test_set.dictionary)
+def test_remove_element(sample_set):
+    sample_set.remove(2)
+    assert not sample_set.contains(2)
 
-    def test_add(self):
-        '''Test add() to set'''
-        test_set = MySet([1,2,3,4])
-        test_set.add(5)
-        set_list = [1,2,3,4,5]
-        for num in set_list:
-            assert(num in test_set.dictionary)
+def test_union(sample_set):
+    other_set = MySet()
+    other_set.add(2)
+    other_set.add(3)
+    other_set.add(4)
+    
+    result_set = sample_set.union(other_set)
+    
+    assert result_set.contains(1)
+    assert result_set.contains(2)
+    assert result_set.contains(3)
+    assert result_set.contains(4)
 
-    def test_delete(self):
-        '''Test delete()'''
-        test_set = MySet([1,2,3,4])
-        test_set.delete(2)
-        set_list = [1,3,4]
-        for num in set_list:
-            assert(num in test_set.dictionary)
+def test_intersection(sample_set):
+    other_set = MySet()
+    other_set.add(2)
+    other_set.add(3)
+    other_set.add(4)
 
-    def test_has(self):
-        '''Test has()'''
-        test_set = MySet([1,2,3,4])
-        assert(test_set.has(1) == True)
-        assert(test_set.has(7) == False)
+    result_set = sample_set.intersection(other_set)
 
-    def test_size(self):
-        '''Test size()'''
-        test_set = MySet([1,2,3,4])
-        assert(len(test_set.dictionary) == 4)
+    assert not result_set.contains(1)
+    assert result_set.contains(2)
+    assert result_set.contains(3)
+    assert not result_set.contains(4)
 
+def test_contains(sample_set):
+    assert sample_set.contains(1)
+    assert not sample_set.contains(4)
 
-
-# Bonus test
-"""
-    def test_clear(self):
-        '''Test clearing set'''
-        test_set = MySet([1,2,3,4])
-        test_set.clear()
-        assert(len(test_set.dictionary) == 0)
-
-    def test_str(self):
-        '''Test __str__()'''
-        test_set = MySet([1,2,3,4])
-        assert(str(test_set) == 'MySet: {1,2,3,4}')
-"""
+if __name__ == "__main__":
+    pytest.main()
